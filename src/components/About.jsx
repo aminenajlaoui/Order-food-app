cd7c35d0728ca500184fa5517b9ea8f1d98e3af7
unknown <anajlaoui007@gmail.com>
2022-11-17 10:39:15 +0100

    add my project mern

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+import aboutImage from "../assets/images/about-image.png";
+
+export const About = () => {
+
+    return (
+        <div className="bg-white">
+            <div className="p-24 grid grid-cols-2">
+                <div className="">
+                    <h2 className="text-2xl font-medium">About Us</h2>
+                    <p className="text-lg">
+                    Welcome,
+                     dear customers,
+                      to our elegant restaurant,
+Here you will find your complete comfort
+ with a list of delicious and luxurious food and drinks and with the best services you can find.
+We are the original and the rest is tradition
+                    </p>
+                </div>
+                <div className="flex items-center justify-center">
+                    <img src={aboutImage} alt="" className="w-[400px] h-[400px] object-cover" />
+                </div>
+            </div>
+        </div>
+    )
+}
